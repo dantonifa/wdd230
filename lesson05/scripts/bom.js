0b30392af0bb0e67c8fd01f49ae2e4e6e1daa47e
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2024-04-06 17:33:20 -0600

    Update bom.js

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -1,22 +1,60 @@
-<!DOCTYPE html>
-<html lang="en-US">
-    <head>
-        <meta charset = "UTF-8">
-        <meta name = "description" content="05: Book of Mormon Favorite Chapters Application">
-        <meta name = "author" content="David Antonio Fajardo Ponce">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>"Book of Mormon - Top 10"</title>
-        <link rel="stylesheet" href="styles/bom.css">
-        <script src="scripts/bom.js"></script>
-    </head>
-    <body>
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("❌​");
+  span.className = "close";
+  span.appendChild(txt);
 
-        <div id="myDIV" class="header">
-          <h2 class="todo-list-title">📖 Book of Mormon - Top 10</h2>
-          <input type="text" id="favchap" placeholder="Alma 5">
-          <span onclick="newElement()" class="addBtn">Add</span>
-        </div>
-        
-        <ul id="myUL"></ul>
-    </body>
-</html>
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("favchap").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("favchap").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("❌​");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+}
