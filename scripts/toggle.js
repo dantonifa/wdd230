da7190f9d2d6c3cb987266eb7226234322238b34
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2024-04-10 06:50:40 -0600

    Add files via upload

---
+++ scripts/toggle.js
@@ -0,0 +1,8 @@
+function toggle(menu) {
+  var element = document.querySelector("#menu");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+}
