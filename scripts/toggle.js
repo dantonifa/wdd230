01169255e60315fbc7c2c38706d301453ba14d35
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2024-04-11 21:03:52 -0600

    Update toggle.js

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,8 +1,14 @@
-function toggle(menu) {
+function toggle() {
   var element = document.querySelector("#menu");
+  var cross = document.querySelector("#close_menu");
+  var hamb_button = document.querySelector("#hamburger_menu");
   if (element.style.display === "none") {
+    hamb_button.style.display = "none";
     element.style.display = "block";
+    cross.style.display = "block";
   } else {
     element.style.display = "none";
+    cross.style.display = "none";
+    hamb_button.style.display = "block";
   }
 }
