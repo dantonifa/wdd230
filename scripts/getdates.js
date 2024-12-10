6f16be19296f979cf5852cb74a6bfdee9894c2e9
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2024-12-09 20:54:43 -0600

    Update getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,40 +1,8 @@
-function lastModified() {
-  var lastMod = new Date(document.lastModified);
-  var day = lastMod.getDay();
-  var month = lastMod.getMonth() + 1;
-  var year = lastMod.getFullYear();
-  var date = lastMod.getDate();
-  var hour = lastMod.getHours();
-  var minute = lastMod.getMinutes();
-  var second = lastMod.getSeconds();
-  var ampm = "AM";
-  if (hour > 12) {
-    hour -= 12;
-    ampm = "PM";
-  }
-  if (hour == 0) {
-    hour = 12;
-  }
-  if (minute < 10) {
-    minute = "0" + minute;
-  }
-  if (second < 10) {
-    second = "0" + second;
-  }
+function getDates() {
+  var date = new Date();
+  let year = date.getFullYear();
+  document.getElementById("currentyear").innerHTML = year;
+  document.getElementById("lastModified").innerHTML = date;
 }
-document.write(
-  "Last Updated: " +
-    month +
-    "/" +
-    date +
-    "/" +
-    year +
-    " " +
-    hour +
-    ": " +
-    minute +
-    " " +
-    ampm +
-    " "
-);
+getDates();
 
